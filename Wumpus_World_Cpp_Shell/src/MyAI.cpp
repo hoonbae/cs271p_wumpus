6bdcc955d73942f7978d43e4e3fe13c74d155d77
hoonbae <hoonbae244@gmail.com>
2018-03-02 17:50:21 -0800

    Agent navigates perimeter and goes home.

--- Wumpus_World_Cpp_Shell/src/MyAI.cpp
+++ Wumpus_World_Cpp_Shell/src/MyAI.cpp
@@ -32,6 +32,8 @@ MyAI::MyAI() : Agent(), m_cur_location(0, 0)
     m_dir_cw = true;
     m_pass_home = false;
     m_switch_dir = 0;
+    m_first_action = true;
+    m_change_dir = false;
 
 }
 
@@ -50,18 +52,40 @@ Agent::Action MyAI::getAction
     set_breeze(breeze);
     // =====================================
 
-  bool go_home_mode = get_go_home_mode();
-  if (!go_home_mode) {
-    if (get_dir_cw_status()) {
+  if (m_first_action) {
+    m_first_action = false;
+
+    if (stench || breeze) {
+      return Agent::CLIMB;
+    }
+
+    if (glitter) {
+      m_gold_status = true;
+      m_actions_taken.push_back(Agent::CLIMB);
+      return Agent::GRAB;
+    }
+  }
+
+  if (!m_go_home_mode) {
+    if (m_dir_cw) {
       return move_cw(stench, breeze, glitter, bump, scream);
     } else {
+      if (m_change_dir) {
+        return move_turnaround(bump);
+      }
       return move_countercw(stench, breeze, glitter, bump, scream);
     }
   } else {
+    if (m_change_dir) {
+      return move_turntohome();
+    }
     return go_home();
   }
 
+  // =====================================
+  // =====================================
 }
+
 
 // Go Home Actions
 Agent::Action MyAI::go_home() {
@@ -71,125 +95,90 @@ Agent::Action MyAI::go_home() {
       if (last_action_taken == Action::TURN_LEFT) return Action::TURN_RIGHT;
       else if(last_action_taken == Action::TURN_RIGHT) return Action::TURN_LEFT;
       return last_action_taken;
+    } else {
+      return Action::CLIMB;
     }
+}
+
+Agent::Action MyAI::move_turnaround(bool bump) {
+  if (m_switch_dir < 2) {
+    m_switch_dir++;
+    return turn_left();
+  } else {
+    m_change_dir = false;
+    m_switch_dir = 0;
+    return move_forward(bump);
+  }
+}
+
+Agent::Action MyAI::move_turntohome() {
+  if (m_switch_dir < 1) {
+    m_switch_dir++;
+    return Action::TURN_RIGHT;
+  } else {
+    m_change_dir = false;
+    m_switch_dir = 0;
+    return Action::TURN_RIGHT;
+  }
 }
 
 // Exploring Perimeter Functions
 Agent::Action MyAI::move_cw(bool stench, bool breeze, bool glitter, bool bump, bool scream) {
+  if (glitter) {
+    m_gold_status = true;
+    m_change_dir = true;
+    m_go_home_mode = true;
+    return Action::GRAB;
+  }
   if (stench || breeze) {
-    is_dir_cw(false);
+    m_dir_cw = false;
+    m_change_dir = true;
     return Action::SHOOT;
   }
   if (bump) {
-    int bump_turn_count = get_bump_turn_count();
-    if(bump_turn_count == 3) {
+    if(m_bump_turn_count == 3) {
       return Action::CLIMB;
     }
-    increment_bump_turn_count();
-    return turn_right();
-  }
-  if (glitter) {
-    set_gold_to_true();
-    is_go_home_mode(true);
-    return Action::GRAB;
+    m_bump_turn_count++;
+    return turn_left();
   }
   return move_forward(bump);
 }
 
 Agent::Action MyAI::move_countercw(bool stench, bool breeze, bool glitter, bool bump, bool scream) {
-  int switch_dir = get_switch_dir();
-  if (switch_dir < 2) {
-    increment_switch_dir();
-    return turn_left();
-  }
-
-  if (stench || breeze) {
-    is_go_home_mode(true);
-    return Action::SHOOT;
-  }
-  if (bump) {
-    int bump_turn_count = get_bump_turn_count();
-    bool pass_home = get_pass_home();
-
-    // moving counter clockwise but haven't passed by home yet
-    if (bump_turn_count > 0 && !pass_home) {
-      decrement_bump_turn_count();
-      return turn_left();
+    if (glitter) {
+      m_gold_status = true;
+      m_change_dir = true;
+      m_go_home_mode = true;
+      return Action::GRAB;
     }
-    // moving counter clockwise - landed home, first time passing
-    if (bump_turn_count == 0 && !pass_home) {
-      increment_bump_turn_count();
-      is_pass_home(true);
-      m_actions_taken.push_back(Action::CLIMB);
-      return turn_left();
+    if (stench || breeze) {
+      m_change_dir = true;
+      m_go_home_mode = true;
+      return Action::SHOOT;
     }
-    // moving counter clockwise - have passed home
-    if (bump_turn_count > 0 && pass_home) {
-      increment_bump_turn_count();
-      return turn_left();
+    if (bump) {
+      // moving counter clockwise but haven't passed by home yet
+      if (m_bump_turn_count > 0 && !m_pass_home) {
+        m_bump_turn_count--;
+        return turn_right();
+      }
+      // moving counter clockwise - landed home, first time passing
+      if (m_bump_turn_count == 0 && !m_pass_home) {
+        m_bump_turn_count++;
+        m_pass_home = true;
+        m_actions_taken.push_back(Action::CLIMB);
+        return turn_right();
+      }
+      // moving counter clockwise - have passed home
+      if (m_bump_turn_count > 0 && m_pass_home) {
+        m_bump_turn_count++;
+        return turn_right();
+      }
     }
-  }
-  if (glitter) {
-    set_gold_to_true();
-    is_go_home_mode(true);
-    return Action::GRAB;
-  }
-  return move_forward(bump);
+    return move_forward(bump);
 }
 
-// Setters
-void MyAI::set_shot_to_true()
-{
-	m_shot_status = true;
-}
-void MyAI::set_gold_to_true()
-{
-	m_gold_status = true;
-}
-void  MyAI::increment_bump_turn_count() {
-  m_bump_turn_count++;
-}
-void  MyAI::decrement_bump_turn_count() {
-  m_bump_turn_count--;
-}
-void  MyAI::is_dir_cw(bool is_clockwise) {
-  m_dir_cw = is_clockwise;
-}
-void  MyAI::is_go_home_mode(bool is_home) {
-  m_go_home_mode = is_home;
-}
-void MyAI::is_pass_home(bool pass_home) {
-  m_pass_home = pass_home;
-}
-void MyAI::increment_switch_dir() {
-  m_switch_dir++;
-}
-
-
-// Getters
-bool MyAI::get_shot_status()
-{
-	return m_shot_status;
-}
-bool MyAI::get_gold_status()
-{
-	return m_gold_status;
-}
-int   MyAI::get_bump_turn_count() {
-  return m_bump_turn_count;
-}
-bool  MyAI::get_dir_cw_status() {
-  return m_dir_cw;
-}
-bool  MyAI::get_go_home_mode() {
-  return m_go_home_mode;
-}
-bool MyAI::get_pass_home() {
-  return m_pass_home;
-}
-int MyAI::get_switch_dir() {
-  return m_switch_dir;
-}
 
 const Board& MyAI::get_board() {
 	return m_board;
